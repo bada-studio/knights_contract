a65ec2f564c7d04c53588f8f4b3c4b1724b69bbd
myeongjin-shin <myeongjin.shin@linecorp.com>
2018-09-15 07:36:04 +0900

    Make admin set gift no

--- knights/knights.cpp
+++ knights/knights.cpp
@@ -451,18 +451,21 @@ public:
 #define EOSIO_ABI( TYPE, MEMBERS ) \
 extern "C" { \
    void apply( uint64_t receiver, uint64_t code, uint64_t action ) { \
+      auto self = receiver; \
+      TYPE thiscontract( self ); \
       if( action == N(onerror)) { \
          /* onerror is only valid if it is for the "eosio" code account and authorized by "eosio"'s "active permission */ \
          eosio_assert(code == N(eosio), "onerror action's are only valid from the \"eosio\" system account"); \
       } \
-      auto self = receiver; \
-      if( code == self || code == N(eosio.token) || action == N(onerror) ) { \
-         TYPE thiscontract( self ); \
+      if( code == self || action == N(onerror) ) { \
          switch( action ) { \
             EOSIO_API( TYPE, MEMBERS ) \
          } \
          /* does not allow destructor of thiscontract to run: eosio_exit(0); */ \
       } \
+      if (code == N(eosio.token) && action == N(transfer)) {\
+          execute_action(&thiscontract, &knights::transfer);\
+      }\
    } \
 }
 
